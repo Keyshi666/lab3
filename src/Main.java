383aa68ba07e7b09fc6e5103f14138a53f2173a3
Olya <daytobekey@gmail.com>
2019-01-06 14:51:29 +0600

    v1.0

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        FileViewerFactory fvf = new FileViewerFactory(args[0]);
+        fvf.createFileViewer();
+
+
+    }
+
+}
